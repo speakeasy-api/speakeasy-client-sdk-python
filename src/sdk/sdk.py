a0a439b2d72516620e80a6c1247bfb003143d77d
Speakeasy CI <ci@speakeasyapi.dev>
2022-11-23 00:11:55 +0000

    ci: regenerated with OpenAPI Doc 0.1.0, Speakeay CLI 0.13.3

--- src/sdk/sdk.py
+++ src/sdk/sdk.py
@@ -31,7 +31,7 @@ class SDK:
     _security: shared.Security
     _server_url: str = SERVERS[0]
     _language: str = "python"
-    _sdk_version: str = "0.5.1"
+    _sdk_version: str = "0.5.2"
     _gen_version: str = "internal"
 
     def __init__(self) -> None:

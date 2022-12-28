d9fafbdece43fd78c4b3204220af4f2fc2c32814
Speakeasy CI <ci@speakeasyapi.dev>
2022-12-28 00:10:32 +0000

    ci: regenerated with OpenAPI Doc 0.1.0, Speakeay CLI 0.16.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="speakeasy-client-sdk-python",
-    version="0.8.0",
+    version="0.8.1",
     author="Speakeasy",
     description="Speakeasy API Client SDK for Python",
     long_description=long_description,
